9890427fb8048b5113e2a3a88727375d0e862977
ivhart <ivhart77@gmail.com>
2021-10-31 18:36:11 +0100

    removed on off description

--- src/Components/Light/index.jsx
+++ src/Components/Light/index.jsx
@@ -3,36 +3,6 @@ import React, { useState } from 'react';
 import offUrl from './light-off.svg';
 import onUrl from './light-on.svg';
 
-/*const Light = (props) => {
-  const [jeZapnuta, setJeZapnuta] = useState(state);
-
-  const handleClick = () => {
-    setJeZapnuta(true);
-  };
-};
-
-return (
-  <>
-    <div className="light">
-      <button className="light__button" disabled={isOn} onClick={handleClick}>
-        <img
-          src={isOn ? onUrl : offUrl}
-          width={80}
-          height={80}
-          alt={isOn ? 'Light on' : 'Light off'}
-          className="light__icon"
-        />
-
-        <div className="light__name">
-          <p>{props.name}</p>
-          <p>{props.state}</p>
-        </div>
-      </button>
-    </div>
-  </>
-);
-*/
-
 const Light = (props) => {
   const [isOn, setIsOn] = useState(resolveOnOff(props.state));
 
@@ -50,7 +20,6 @@ const Light = (props) => {
         <button onClick={handleClick}>
           <img src={isOn ? onUrl : offUrl} />
           <p>{props.name}</p>
-          <p>{props.state}</p>
         </button>
       </div>
     </>
