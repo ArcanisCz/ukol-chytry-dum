40bb61dbe261eecc6913df0675b11c3283bb3d3a
Baska <baska.klimekova@gmail.com>
2021-11-10 22:15:24 +0100

    mporting smartHomeData.js into main index.jsx (task 6)

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
 
+import Header from './components/Header';
+import Dashboard from './components/Dashboard';
+
+import smartHomeData from './smartHomeData';
+
 const App = () => (
   <>
-
+    <div className="container">
+      <Header className= "header"  title="Chytrý dům"/>
+      <Dashboard data={smartHomeData} />
+    </div>
   </>
 );
 
