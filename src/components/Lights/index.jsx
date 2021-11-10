8487fe20081541829dc947aa2b2ec7ad2242b4f3
Baska <baska.klimekova@gmail.com>
2021-11-10 22:31:50 +0100

    create Lights component

---
+++ src/components/Lights/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Light = () => {
+    
+    return (
+        <>
+            <div className="light" >
+					
+			</div>      
+        </>
+    );
+    
+}
+
+export default Light;
