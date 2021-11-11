c24df6ae7046062fea5f88b03ba6228f76503196
Baska <baska.klimekova@gmail.com>
2021-11-11 08:34:08 +0100

    define Light component

--- src/components/Light/index.jsx
+++ src/components/Light/index.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 
-const Light = () => {
+import lightOn from './light-on.svg';
+import lightOff from './light-off.svg';
+
+
+const Light = ({name, state}) => {
+   
+    const [lightState, setLightState] = useState(state);
+    const switchLight = () => setLightState(lightState => (lightState === 'on' ? 'off' : 'on'));
     
     return (
         <>
-            <div className="light" >
-					
+            <div onClick={switchLight} className="light" >
+                <div className="light__icon">
+                    <img src={(lightState === 'on' ? lightOn : lightOff)} />
+                </div>
+                <div className="light__name">{name}</div>	
 			</div>      
         </>
-    );
-    
+    ); 
 }
 
 export default Light;
