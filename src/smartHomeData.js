33ffdc9ab25aacafd335e147cffb3add9b95dfd7
Evita-M <eva.matova@seznam.cz>
2022-04-02 20:30:24 +0200

    Make minor changes in smartHomeData

--- src/smartHomeData.js
+++ src/smartHomeData.js
@@ -1,31 +1,55 @@
 export const smartHomeData = {
-	lights: [
-		{
-			name: 'Obývací pokoj',
-			state: 'off',
-		},
-		{
-			name: 'Ložnice',
-			state: 'on',
-		},
-		{
-			name: 'Kuchyně',
-			state: 'on',
-		},
-		{
-			name: 'Chodba',
-			state: 'off',
-		},
-	],
-	climate: {
-		temperature: 24,
-		humidity: 50,
-	},
-	blinds: 'open',
-	energyConsumption: {
-		electricity: 36.7,
-		water: 14.1,
-	}
+  lights: [
+    {
+      id: 1,
+      name: "Obývací pokoj",
+      state: "off",
+    },
+    {
+      id: 2,
+      name: "Ložnice",
+      state: "on",
+    },
+    {
+      id: 3,
+      name: "Kuchyně",
+      state: "on",
+    },
+    {
+      id: 4,
+      name: "Chodba",
+      state: "off",
+    },
+    {
+      id: 5,
+      name: "Dětský pokoj",
+      state: "off",
+    },
+    {
+      id: 6,
+      name: "Jídelna",
+      state: "on",
+    },
+  ],
+  climate: {
+    temperature: 24,
+    humidity: 50,
+  },
+  blinds: "open",
+  energyConsumption: [
+    {
+      img: "./assets/electricity.svg",
+      label: "Elektřina",
+      value: 36.7,
+      unit: "kW/h",
+    },
+    {
+      img: "./assets/water.svg",
+      label: "Voda",
+      value: 14.1,
+      unit: "m3",
+    },
+  ],
 };
 
 export default smartHomeData;
