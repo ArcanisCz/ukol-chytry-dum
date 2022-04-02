d24c7b6abb03f9de07dcf1af97a61d32817ce01f
Evita-M <eva.matova@seznam.cz>
2022-04-02 20:31:19 +0200

    Make changes in index.js and global style and images

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import { render } from 'react-dom';
-import './style.css';
+import React from "react";
+import { render } from "react-dom";
+import Header from "./components/header/Header";
+import Dashboard from "./components/dashboard/Dashboard";
+import smartHomeData from "./smartHomeData";
+import "./style.css";
 
-const App = () => (
-  <>
+const App = () => {
+  return (
+    <div className="container">
+      <Header title="Chytrý dům" />
+      <Dashboard data={smartHomeData} />
+    </div>
+  );
+};
 
-  </>
-);
-
-render(<App />, document.querySelector('#app'));
+render(<App />, document.querySelector("#app"));
