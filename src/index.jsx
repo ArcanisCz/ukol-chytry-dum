8f176b415106ee7663eb02b98260171d305b4015
Jirinka <jirina.poustecka@greenfoxacademy.com>
2022-04-11 23:56:26 +0200

    add Header

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
+import Header from './components/Header';
 
 const App = () => (
   <>
+    <div class="container">
+      <Header title="Chytrý dům" />
 
+    </div>
   </>
 );
 
